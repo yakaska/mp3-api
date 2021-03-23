d6a39d6cbc75abb62217fbbb9adbefc63cd0f11d
Valentin <valentin7petr@gmail.com>
2021-03-23 17:59:11 +0300

    token

--- app.py
+++ app.py
@@ -1,49 +1,16 @@
-import socket
-
-import vkaudiotoken
-from flask import Flask, jsonify, request
-from vk_api import vk_api
-from vk_api.audio import VkAudio
+from flask import Flask, request
 from vkaudiotoken import get_vk_official_token
-
-from Audio import Track
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return "Home"
-
-
-@app.route('/token', methods=['POST', 'GET'])
+@app.route('/token.get', methods=['POST'])
 def get_token():
     login = request.args.get('login')
     password = request.args.get('password')
     token = get_vk_official_token(login=login, password=password)
-    print(token)
     return token
 
 
-@app.route('/audio', methods=['POST', 'GET'])
-def get_audio():
-    login = request.args.get('login')
-    token = request.args.get('token')
-    vk_session = vk_api.VkApi(login=login, token=token)
-    try:
-        vk_session.auth(token_only=True)
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
-    vk_audio = VkAudio(vk_session)
-    tracks = vk_audio.get_iter()
-    songs = []
-    for n, track in enumerate(tracks, 1):
-        songs.append(Track(track['title'], track['artist'], track['url'], track['track_covers'],
-                           track['duration'] * 1000).serialize())
-    tracks.close()
-    return jsonify(songs)
-
-#
 if __name__ == '__app__':
     app.run()
