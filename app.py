ea80c003a55331cdc3f078442e63478bb596f556
Valentin <valentin7petr@gmail.com>
2021-03-24 17:08:39 +0300

    token

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/token.get', methods=['GET', 'POST'])
 def get_token():
-    login = request.args.get('login')
-    password = request.args.get('password')
+    login = str(request.args.get('login'))
+    password = str(request.args.get('password'))
     try:
         token = get_vk_official_token(login=login, password=password)
     except TokenException as ex:
